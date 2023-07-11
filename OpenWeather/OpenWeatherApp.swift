090c6d25cd823295e9a8661df8084686d81e3489
Prangbi <prangbi@gmail.com>
2023-07-11 23:51:22 +0900

    Initial Commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by 구홍석 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
